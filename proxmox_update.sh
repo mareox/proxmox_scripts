da5cd4eb9e0b07f890779ad529cf9107402134a1
mareox <mareox@gmail.com>
2023-05-07 02:42:13 -0700

    Update proxmox_update.sh

--- proxmox_update.sh
+++ proxmox_update.sh
@@ -3,7 +3,7 @@
 # Disable Commercial Repo
 sed -i "s/^deb/\#deb/" /etc/apt/sources.list.d/pve-enterprise.list
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt update -y
+apt update -y && apt upgrade -y && apt update -y
 
 # Add PVE Community Repo
 echo "deb http://download.proxmox.com/debian/pve $(grep "VERSION=" /etc/os-release | sed -n 's/.*(\(.*\)).*/\1/p') pve-no-subscription" > /etc/apt/sources.list.d/pve-no-enterprise.list 
